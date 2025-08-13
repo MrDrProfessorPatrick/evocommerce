30c8264e9a539515d8d72b3e46048848664909ae
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-08-13 18:35:01 +0300

    style for chosein button added to ChangeGoodsLayoutButtons

--- src/components/ChangeGoodsLayoutButtons.js
+++ src/components/ChangeGoodsLayoutButtons.js
@@ -4,13 +4,17 @@ import { Context } from "@/app/providers";
 
 export default function ChangeGoodsLayoutButtons() {
   const goodsLayouts = ["big", "medium", "small"];
-  const { changeGoodsLayout } = useContext(Context);
+  const { changeGoodsLayout, goodsLayout } = useContext(Context);
 
   return (
     <div className="flex justify-end items-end h-40px space-x-4 pb-5 pt-6 pr-8">
       {goodsLayouts.map((name) => (
         <button
-          className="text-white border border-white hover:bg-white hover:text-black transition cursor-pointer pb-3 pt-3 pr-5 pl-5 rounded-md"
+          className={` ${
+            goodsLayout.view === name
+              ? "bg-white text-black"
+              : "text-white border border-white hover:bg-white hover:text-black"
+          } transition cursor-pointer pb-3 pt-3 pr-5 pl-5 rounded-md`}
           onClick={() => changeGoodsLayout(name)}
           key={name}
         >
